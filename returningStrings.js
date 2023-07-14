7e13831dc1874ccc84f1df030f7447f3039cfcde
Stephon <stephonhorton222@gmail.com>
2023-07-14 06:13:35 -0400

    codewars returning strings solution

---
+++ returningStrings.js
@@ -0,0 +1,15 @@
+/*  Question ~ Returning Strings :
+Make a function that will return a greeting statement that uses an input; your program should return,
+ "Hello, <name> how are you doing today?".
+
+ Example Returns :
+(stephon)  ==>  "Hello, stephon how are you doing today?"
+(james)  ==>  "Hello, james how are you doing today?"
+(dan)  ==>  "Hello, dan how are you doing today?"
+*/
+
+// Answer :
+
+function greet(name){
+  return `Hello, ${name} how are you doing today?`
+};
---
+++ youCantCodeUnderPressure#1.js
@@ -0,0 +1,16 @@
+/*  Question ~ You Can't Code Under Pressure #1 :
+Code as fast as you can! You need to double the integer and return it.
+
+
+Example Returns :
+(2)  ==>  4
+(16)  ==>  32
+(100)  ==>  200
+*/
+
+// Answer : 
+
+function doubleInteger(i) {
+  i *= 2
+  return i;
+}
