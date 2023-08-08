11b9eddad5f1a96aa68c0170ccceed26d07b9713
Stephon <stephonhorton222@gmail.com>
2023-08-08 09:44:34 -0400

    codewars Grasshopper-Summation solution

--- grasshopper-Summation.js
+++ grasshopper-Summation.js
@@ -1,3 +1,14 @@
+/* Question ~ Grasshopper-Summation: Write a program that finds the summation of every number
+ from 1 to num. The number will always be a positive integer greater than 0.
+
+ Example returns:
+ (input -> output)
+2 -> 3 (1 + 2)
+8 -> 36 (1 + 2 + 3 + 4 + 5 + 6 + 7 + 8)
+
+Answer:
+*/
+
 var summation = function (num) {
   let sum = 0;
   for(let i = 1; i <= num; i++){
