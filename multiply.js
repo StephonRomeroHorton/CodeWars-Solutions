10b198c37e7d7fa4ad687c70fc237fdf024a74dd
Stephon <stephonhorton222@gmail.com>
2023-09-05 08:26:19 -0400

    codewars multiply solution

---
+++ multiply.js
@@ -0,0 +1,12 @@
+/* Question ~ Multiply: This code does not execute properly. Try to figure out why.
+
+function multiply(a, b){
+  a * b
+}
+
+Answer:
+*/
+
+function multiply(a, b){
+  return a * b
+}
