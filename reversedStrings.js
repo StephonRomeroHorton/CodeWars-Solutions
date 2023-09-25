7d84f23c24b71e2bc035a85938368cbf769211b3
Stephon <stephonhorton222@gmail.com>
2023-09-25 08:49:33 -0400

    codewars reversed strings solution

---
+++ reversedStrings.js
@@ -0,0 +1,11 @@
+/* Question ~ Reversed Strings: Complete the solution so that it reverses the string passed into it.
+
+Exanples:
+
+'world'  =>  'dlrow'
+'word'   =>  'drow'
+
+Answer:
+*/
+
+const solution = str =>  str.split('').reverse().join('');
